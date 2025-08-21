949e8922244134688957b7020258b0600761ddda
Ransika L.G.C <chamudiransika@gmail.com>
2025-08-21 23:17:32 +0530

    Guided project update

---
+++ Tool calling Agent.py
@@ -0,0 +1,27 @@
+import re
+from pytube import YouTube
+from langchain_core.tools import tool
+from IPython.display import display, JSON
+import yt_dlp
+from typing import List, Dict
+from langchain_core.messages import HumanMessage
+from langchain_core.messages import ToolMessage
+import json
+
+# Suppress warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+# Suppress pytube errors
+import logging
+pytube_logger = logging.getLogger('pytube')
+pytube_logger.setLevel(logging.ERROR)
+
+# Suppress yt-dlp warnings
+yt_dpl_logger = logging.getLogger('yt_dlp')
+yt_dpl_logger.setLevel(logging.ERROR)
+
+from langchain.chat_models import init_chat_model
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+os.environ["OPENAI_API_KEY"] = "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"
